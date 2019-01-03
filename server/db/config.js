42426da7f74bf796c000e09b7d3096c29e161ff8
justinposer <jposer1@gmail.com>
2019-01-03 15:32:13 -0500

    postgres password

--- server/db/config.js
+++ server/db/config.js
@@ -3,7 +3,7 @@ var knex = require("knex")({
   connection: {
     host: "127.0.0.1",
     user: "postgres",
-    password: "Leviathan6",
+    password: process.env.PASSWORD,
     database: "fakeMovies"
   },
   pool: { min: 0, max: 7 }
